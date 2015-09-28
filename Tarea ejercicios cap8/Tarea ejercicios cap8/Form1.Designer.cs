28ccd7e2b72a620d6cd42002e6008fe8d4fffb03
hamlet3 <hamletreyes3@gmail.com>
2015-09-28 15:53:44 -0600

    Tarea

---
+++ Tarea ejercicios cap8/Tarea ejercicios cap8/Form1.Designer.cs
@@ -0,0 +1,429 @@
+﻿namespace Tarea_ejercicios_cap8
+{
+    partial class TareaCap8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TCtarea8 = new System.Windows.Forms.TabControl();
+            this.tabCadena = new System.Windows.Forms.TabPage();
+            this.tabJustificacion = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCadena = new System.Windows.Forms.TextBox();
+            this.labelCadena = new System.Windows.Forms.Label();
+            this.btnProcesar1 = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtCadena2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDerecha = new System.Windows.Forms.TextBox();
+            this.txtIzquierda = new System.Windows.Forms.TextBox();
+            this.labelCadena2 = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.tabfecha = new System.Windows.Forms.TabPage();
+            this.labelTiempo = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabSuma = new System.Windows.Forms.TabPage();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelSuma = new System.Windows.Forms.Label();
+            this.TCtarea8.SuspendLayout();
+            this.tabCadena.SuspendLayout();
+            this.tabJustificacion.SuspendLayout();
+            this.tabfecha.SuspendLayout();
+            this.tabSuma.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TCtarea8
+            // 
+            this.TCtarea8.Controls.Add(this.tabCadena);
+            this.TCtarea8.Controls.Add(this.tabJustificacion);
+            this.TCtarea8.Controls.Add(this.tabfecha);
+            this.TCtarea8.Controls.Add(this.tabSuma);
+            this.TCtarea8.Location = new System.Drawing.Point(0, 1);
+            this.TCtarea8.Name = "TCtarea8";
+            this.TCtarea8.SelectedIndex = 0;
+            this.TCtarea8.Size = new System.Drawing.Size(295, 268);
+            this.TCtarea8.TabIndex = 0;
+            // 
+            // tabCadena
+            // 
+            this.tabCadena.Controls.Add(this.btnProcesar1);
+            this.tabCadena.Controls.Add(this.labelCadena);
+            this.tabCadena.Controls.Add(this.txtCadena);
+            this.tabCadena.Controls.Add(this.label1);
+            this.tabCadena.Location = new System.Drawing.Point(4, 22);
+            this.tabCadena.Name = "tabCadena";
+            this.tabCadena.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCadena.Size = new System.Drawing.Size(287, 242);
+            this.tabCadena.TabIndex = 0;
+            this.tabCadena.Text = "Cadena";
+            this.tabCadena.UseVisualStyleBackColor = true;
+            // 
+            // tabJustificacion
+            // 
+            this.tabJustificacion.Controls.Add(this.btnMostrar);
+            this.tabJustificacion.Controls.Add(this.labelCadena2);
+            this.tabJustificacion.Controls.Add(this.txtIzquierda);
+            this.tabJustificacion.Controls.Add(this.txtDerecha);
+            this.tabJustificacion.Controls.Add(this.label5);
+            this.tabJustificacion.Controls.Add(this.label4);
+            this.tabJustificacion.Controls.Add(this.label3);
+            this.tabJustificacion.Controls.Add(this.btnIngresar);
+            this.tabJustificacion.Controls.Add(this.txtCadena2);
+            this.tabJustificacion.Controls.Add(this.label2);
+            this.tabJustificacion.Location = new System.Drawing.Point(4, 22);
+            this.tabJustificacion.Name = "tabJustificacion";
+            this.tabJustificacion.Padding = new System.Windows.Forms.Padding(3);
+            this.tabJustificacion.Size = new System.Drawing.Size(287, 242);
+            this.tabJustificacion.TabIndex = 1;
+            this.tabJustificacion.Text = "Justificacion";
+            this.tabJustificacion.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escriba una cadena";
+            // 
+            // txtCadena
+            // 
+            this.txtCadena.Location = new System.Drawing.Point(84, 51);
+            this.txtCadena.Name = "txtCadena";
+            this.txtCadena.Size = new System.Drawing.Size(100, 20);
+            this.txtCadena.TabIndex = 1;
+            // 
+            // labelCadena
+            // 
+            this.labelCadena.AutoSize = true;
+            this.labelCadena.Location = new System.Drawing.Point(64, 111);
+            this.labelCadena.Name = "labelCadena";
+            this.labelCadena.Size = new System.Drawing.Size(0, 13);
+            this.labelCadena.TabIndex = 2;
+            // 
+            // btnProcesar1
+            // 
+            this.btnProcesar1.Location = new System.Drawing.Point(91, 77);
+            this.btnProcesar1.Name = "btnProcesar1";
+            this.btnProcesar1.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar1.TabIndex = 3;
+            this.btnProcesar1.Text = "Procesar";
+            this.btnProcesar1.UseVisualStyleBackColor = true;
+            this.btnProcesar1.Click += new System.EventHandler(this.btnProcesar1_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(92, 68);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 6;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // txtCadena2
+            // 
+            this.txtCadena2.Location = new System.Drawing.Point(81, 42);
+            this.txtCadena2.Name = "txtCadena2";
+            this.txtCadena2.Size = new System.Drawing.Size(100, 20);
+            this.txtCadena2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Escriba una cadena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Como desea justificarlo?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Pasos a la izquierda";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Pasos a la derecha";
+            // 
+            // txtDerecha
+            // 
+            this.txtDerecha.Location = new System.Drawing.Point(192, 156);
+            this.txtDerecha.Name = "txtDerecha";
+            this.txtDerecha.Size = new System.Drawing.Size(44, 20);
+            this.txtDerecha.TabIndex = 10;
+            // 
+            // txtIzquierda
+            // 
+            this.txtIzquierda.Location = new System.Drawing.Point(29, 156);
+            this.txtIzquierda.Name = "txtIzquierda";
+            this.txtIzquierda.Size = new System.Drawing.Size(44, 20);
+            this.txtIzquierda.TabIndex = 11;
+            // 
+            // labelCadena2
+            // 
+            this.labelCadena2.AutoSize = true;
+            this.labelCadena2.Location = new System.Drawing.Point(84, 196);
+            this.labelCadena2.Name = "labelCadena2";
+            this.labelCadena2.Size = new System.Drawing.Size(98, 13);
+            this.labelCadena2.TabIndex = 12;
+            this.labelCadena2.Text = "Pasos a la derecha";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(97, 156);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 13;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // tabfecha
+            // 
+            this.tabfecha.Controls.Add(this.label6);
+            this.tabfecha.Controls.Add(this.btnGenerar);
+            this.tabfecha.Controls.Add(this.labelTiempo);
+            this.tabfecha.Location = new System.Drawing.Point(4, 22);
+            this.tabfecha.Name = "tabfecha";
+            this.tabfecha.Padding = new System.Windows.Forms.Padding(3);
+            this.tabfecha.Size = new System.Drawing.Size(287, 242);
+            this.tabfecha.TabIndex = 2;
+            this.tabfecha.Text = "Fecha";
+            this.tabfecha.UseVisualStyleBackColor = true;
+            // 
+            // labelTiempo
+            // 
+            this.labelTiempo.AutoSize = true;
+            this.labelTiempo.Location = new System.Drawing.Point(87, 125);
+            this.labelTiempo.Name = "labelTiempo";
+            this.labelTiempo.Size = new System.Drawing.Size(0, 13);
+            this.labelTiempo.TabIndex = 0;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(90, 84);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Pulse para generar el tiempo";
+            // 
+            // tabSuma
+            // 
+            this.tabSuma.Controls.Add(this.labelSuma);
+            this.tabSuma.Controls.Add(this.label11);
+            this.tabSuma.Controls.Add(this.label10);
+            this.tabSuma.Controls.Add(this.label9);
+            this.tabSuma.Controls.Add(this.label8);
+            this.tabSuma.Controls.Add(this.txtNumero2);
+            this.tabSuma.Controls.Add(this.btnSumar);
+            this.tabSuma.Controls.Add(this.txtNumero1);
+            this.tabSuma.Controls.Add(this.label7);
+            this.tabSuma.Location = new System.Drawing.Point(4, 22);
+            this.tabSuma.Name = "tabSuma";
+            this.tabSuma.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSuma.Size = new System.Drawing.Size(287, 242);
+            this.tabSuma.TabIndex = 3;
+            this.tabSuma.Text = "Suma";
+            this.tabSuma.UseVisualStyleBackColor = true;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(103, 88);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 6;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(80, 62);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(51, 20);
+            this.txtNumero1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(146, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Numero#2";
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(149, 62);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(51, 20);
+            this.txtNumero2.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(77, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Numero#1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(133, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "+";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(134, 115);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "||";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(133, 126);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "V";
+            // 
+            // labelSuma
+            // 
+            this.labelSuma.AutoSize = true;
+            this.labelSuma.Location = new System.Drawing.Point(131, 140);
+            this.labelSuma.Name = "labelSuma";
+            this.labelSuma.Size = new System.Drawing.Size(14, 13);
+            this.labelSuma.TabIndex = 12;
+            this.labelSuma.Text = "| |";
+            // 
+            // TareaCap8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.TCtarea8);
+            this.MaximizeBox = false;
+            this.Name = "TareaCap8";
+            this.Text = "Tarea cap#8";
+            this.TCtarea8.ResumeLayout(false);
+            this.tabCadena.ResumeLayout(false);
+            this.tabCadena.PerformLayout();
+            this.tabJustificacion.ResumeLayout(false);
+            this.tabJustificacion.PerformLayout();
+            this.tabfecha.ResumeLayout(false);
+            this.tabfecha.PerformLayout();
+            this.tabSuma.ResumeLayout(false);
+            this.tabSuma.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TCtarea8;
+        private System.Windows.Forms.TabPage tabCadena;
+        private System.Windows.Forms.Button btnProcesar1;
+        private System.Windows.Forms.Label labelCadena;
+        private System.Windows.Forms.TextBox txtCadena;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabJustificacion;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label labelCadena2;
+        private System.Windows.Forms.TextBox txtIzquierda;
+        private System.Windows.Forms.TextBox txtDerecha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtCadena2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabfecha;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label labelTiempo;
+        private System.Windows.Forms.TabPage tabSuma;
+        private System.Windows.Forms.Label labelSuma;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Tarea ejercicios cap8/Tarea ejercicios cap8/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_ejercicios_cap8
+{
+    public partial class TareaCap8 : Form
+    {
+        string cadena;
+        string tiempo;
+        public TareaCap8()
+        {
+            InitializeComponent();
+            btnMostrar.Enabled = false;
+        }
+        //cadena
+        private void btnProcesar1_Click(object sender, EventArgs e)
+        {
+            labelCadena.Text = txtCadena.Text; 
+            
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            labelCadena2.Text = txtCadena2.Text;
+            btnMostrar.Enabled = true;
+        }
+        //Justificacion
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (txtDerecha.Text != "")
+            {
+                cadena = labelCadena2.Text.PadLeft(int.Parse(txtDerecha.Text));
+                labelCadena2.Text = cadena;
+            }
+            if (txtIzquierda.Text != "")
+            {//ni idea de por que no se mueve a la izquierda
+                cadena = labelCadena2.Text.PadRight(int.Parse(txtIzquierda.Text));
+                labelCadena2.Text = cadena;
+            }
+           
+        }
+        //Fecha
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+
+            labelTiempo.Text = String.Format("La fecha es :{0:h:m:s tt} \n {0:dddd/ M /yyyy}", DateTime.Now); 
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            float suma= float.Parse(txtNumero1.Text) + float.Parse(txtNumero2.Text);
+            if (suma< 0)
+            {
+                labelSuma.Text = "(" + suma + ")";
+            }
+            else {
+                labelSuma.Text =suma.ToString();
+            }
+        }
+    }
+}
